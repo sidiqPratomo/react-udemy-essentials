7f673e966a4616a0f3935bb609da8ba719c6103d
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-11-06 22:50:04 +0700

    add tab content berdasarkan tab yang dipilih

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,14 @@ import Header from "./component/Header/Header.jsx";
 import CoreConcept from "./component/CoreConcept.jsx";
 import TabButton from "./component/TabButton.jsx";
 import { CORE_CONCEPTS } from "./data.js";
+import { useState } from "react";
+import { EXAMPLES } from "./data-with-examples.js";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -24,12 +30,19 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
         </section>
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
       </main>
     </div>
   );
